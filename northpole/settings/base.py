5cd997c344199b53d1fc3ec125acab28b915c257
Michael Hotwagner <mhotwagner@gmail.com>
2019-12-05 08:05:26 -0500

    Componetize redis hostname to Env variable

--- northpole/settings/base.py
+++ northpole/settings/base.py
@@ -144,7 +144,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('0.0.0.0', 6379)],
+            "hosts": [(os.getenv('REDIS_HOST', '0.0.0.0'), 6379)],
         },
     },
 }
