6ff64e49bbabe35d3b3bb18bd233d38db4f64d18
Michael Hotwagner <mhotwagner@gmail.com>
2020-01-04 19:32:09 -0500

    Use runserver in stage again, maybe

--- staging-start.sh
+++ staging-start.sh
@@ -24,4 +24,5 @@ echo
 
 echo "Starting server"
 #gunicorn -b 0.0.0.0:8000 northpole.wsgi:application --access-logfile /var/log/northpole-access.log --log-file -
-daphne --port 8000 --access-log /var/log/northpole-access.log northpole.asgi:application
+#daphne --port 8000 --access-log /var/log/northpole-access.log northpole.asgi:application
+python3 manage.py runserver 8000
