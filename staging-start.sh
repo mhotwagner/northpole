25f16d49234474a1e8da6439d0a38cbef9d79774
Michael Hotwagner <mhotwagner@gmail.com>
2020-01-07 15:33:11 -0500

    Explicitly bind to 0.0.0.0

--- staging-start.sh
+++ staging-start.sh
@@ -31,4 +31,4 @@ echo
 echo "Starting server"
 #gunicorn -b 0.0.0.0:8000 northpole.wsgi:application --access-logfile /var/log/northpole-access.log --log-file -
 #daphne --port 8000 --access-log /var/log/northpole-access.log northpole.asgi:application
-python3 manage.py runserver 8080
+python3 manage.py runserver 0.0.0.0:8080
