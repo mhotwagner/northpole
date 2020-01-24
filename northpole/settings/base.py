97e806e6969b542b2b9bbbc054eb9165d0caf03e
Michael Hotwagner <mhotwagner@gmail.com>
2020-01-23 21:10:04 -0500

    Add ingress to northpole.yaml

--- northpole/settings/base.py
+++ northpole/settings/base.py
@@ -144,7 +144,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.getenv('REDIS_HOST', '0.0.0.0'), 6379)],
+            "hosts": [(os.getenv('REDIS_HOST', '0.0.0.0'), os.getenv('REDIS_PORT', 6379))],
         },
     },
 }
