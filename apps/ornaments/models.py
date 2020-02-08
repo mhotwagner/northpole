1647d0fcf16bac355d648c695d1ac9eb3e9dab6f
Michael Hotwagner <mhotwagner@gmail.com>
2020-02-08 15:48:29 -0500

    Add Ornament model

--- apps/ornaments/models.py
+++ apps/ornaments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django_extensions.db.models import TimeStampedModel
+
+
+class OrnamentDevice(TimeStampedModel):
+    mac_address = models.CharField(db_index=True, max_length=12)
+
+    def __str__(self):
+        return f'Ornament<{self.mac_address}>'
