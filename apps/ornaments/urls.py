4a522e6f82bfc158831e0e04a7ba301e77526297
Michael Hotwagner <mhotwagner@gmail.com>
2020-02-10 09:05:03 -0500

    fix an import issue

--- apps/ornaments/urls.py
+++ apps/ornaments/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import test_view
+from .views import device_view, controller_view
 
 
 urlpatterns = [
-    path('ornaments/controller/<str:mac_address>/', test_view),
-    path('ornaments/device/<str:mac_address>/', test_view),
+    path('ornaments/controller/<str:mac_address>/', controller_view),
+    path('ornaments/device/<str:mac_address>/', device_view),
 ]
