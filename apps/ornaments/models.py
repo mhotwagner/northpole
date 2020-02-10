9b14eda608040233d568ae11b721e52e11e74a55
Michael Hotwagner <mhotwagner@gmail.com>
2020-02-10 18:14:50 -0500

    Add unique nickname to ornaments

--- apps/ornaments/models.py
+++ apps/ornaments/models.py
@@ -5,6 +5,9 @@ from django_extensions.db.models import TimeStampedModel
 
 class OrnamentDevice(TimeStampedModel):
     mac_address = models.CharField(db_index=True, max_length=12)
+    nickname = models.CharField(max_length=64, unique=True, null=True)
 
     def __str__(self):
+        if self.nickname:
+            return f'Ornament<{self.nickname}>'
         return f'Ornament<{self.mac_address}>'
