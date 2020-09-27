60c903d1b0c478550f0870b64a6ec54edc9965b5
Michael Hotwagner <mhotwagner@gmail.com>
2020-09-27 08:58:09 -0400

    Add logger admin

--- apps/logger/admin.py
+++ apps/logger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Log
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    fields = ('id', 'message')
+
