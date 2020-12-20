c629e6578e2d3e30e015989357ca434be65a95aa
Michael Hotwagner <mhotwagner@gmail.com>
2020-12-19 19:18:51 -0500

    Add whitenoise

--- northpole/settings/staging.py
+++ northpole/settings/staging.py
@@ -16,6 +16,8 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, '..' 'media')
 MEDIA_URL = '/media/'
 
