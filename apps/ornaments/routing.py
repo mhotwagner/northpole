d174b766dd1381ceedf88ae94d9bd723cd34c59a
Michael Hotwagner <mhotwagner@gmail.com>
2020-12-19 20:13:03 -0500

    Hopefully updage websocket consumer routing for Channels with asgi V3

--- apps/ornaments/routing.py
+++ apps/ornaments/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .consumers import OrnamentDeviceConsumer, OrnamentControllerConsumer
 
 websocket_urlpatterns = [
-    path(r'ws/ornaments/controller/<str:mac_address>/', OrnamentControllerConsumer),
-    path(r'ws/ornaments/device/<str:mac_address>/', OrnamentDeviceConsumer),
+    path(r'ws/ornaments/controller/<str:mac_address>/', OrnamentControllerConsumer.as_asgi()),
+    path(r'ws/ornaments/device/<str:mac_address>/', OrnamentDeviceConsumer.as_asgi()),
 ]
